7b9998a7ff1407c14540a430cf9d887c64e2f510
Mallikarjunarao Kosuri <malli.kv2@gmail.com>
2024-10-31 18:02:27 +0530

    feat: allow commit message case insesitive

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,24 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'type-enum': [2, 'always', [
-        'feat',    // A new feature
-        'fix',     // A bug fix
-        'docs',    // Documentation changes
-        'style',   // Code style changes (formatting, whitespace)
-        'refactor', // Code refactoring (no feature or bug fix)
-        'perf',    // Performance improvements
-        'test',    // Adding or fixing tests
-        'build',   // Changes affecting the build system or dependencies
-        'ci',      // Changes to CI/CD configuration
-        'chore',   // Minor changes not impacting code (e.g., tool changes)
-      ]],
-      'subject-case': [2, 'always', 'sentence-case'],
-      'subject-min-length': [2, 'always', 10],
-      'footer-max-line-length': [2, 'always', 100]
-    }
-  };
-  
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat',    // A new feature
+      'fix',     // A bug fix
+      'docs',    // Documentation changes
+      'style',   // Code style changes (formatting, whitespace)
+      'refactor', // Code refactoring (no feature or bug fix)
+      'perf',    // Performance improvements
+      'test',    // Adding or fixing tests
+      'build',   // Changes affecting the build system or dependencies
+      'ci',      // Changes to CI/CD configuration
+      'chore',   // Minor changes not impacting code (e.g., tool changes)
+    ]],
+    'subject-case': [
+      2,
+      'always',
+      ['sentence-case', 'start-case', 'lower-case', 'upper-case']  // Allow multiple cases
+    ],
+    'subject-min-length': [2, 'always', 10],
+    'footer-max-line-length': [2, 'always', 100]
+  }
+};
